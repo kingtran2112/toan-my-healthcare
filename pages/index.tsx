c6a55f10431b740e0904409c54df43090055cca0
King Tran <kingtran.2112@gmail.com>
2022-12-11 20:23:29 +0700

    Add Benefit section

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,10 +23,40 @@ export default function Home() {
           />
         </div>
         <Introduction />
+        <Benefit />
       </main>
     </div>
   );
 }
+
+const Benefit = () => {
+  return (
+    <div id="benefit" className="w-full">
+      <div className="flex flex-col items-center container mx-auto p-12 ">
+        <h1 className="uppercase font-bold text-3xl text-center whitespace-pre-wrap">
+          {content.benefit.heading}
+        </h1>
+        <hr className="w-1/3 border-t-2 border-solid border-current mt-2 mb-6" />
+        <div className="flex w-full flex-wrap justify-evenly">
+          {content.benefit.details.map((detail) => (
+            <div
+              key={detail.title}
+              className="flex flex-shrink-0 w-2/5 my-2 shadow-md rounded-xl overflow-hidden"
+            >
+              <div>
+                <Image src={detail.img} alt="" width={500} height={500} />
+              </div>
+              <div className="px-1">
+                <h2>{detail.title}</h2>
+                <p>{detail.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Introduction = () => {
   return (
